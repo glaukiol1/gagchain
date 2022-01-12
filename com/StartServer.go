de969c6d94c363acb631cc4ff643d7feea5d20c4
glaukiol1 <glaukiol1@gmail.com>
2022-01-12 21:41:35 +0100

    Set StartServer in newfile

---
+++ com/StartServer.go
@@ -0,0 +1,7 @@
+package com
+
+func StartServer() {
+	// runs on MessagePipeStart(func (message string))
+	// used to listen on a port
+	// and wait for messages
+}
--- com/main.go
+++ com/main.go
@@ -1,11 +1,5 @@
 package com
 
-func StartServer() {
-	// runs on MessagePipeStart(func (message string))
-	// used to listen on a port
-	// and wait for messages
-}
-
 func BroadcastMessage(message string) {
 	// connect to the selected node
 	// send the message
