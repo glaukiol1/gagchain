e3f545063b1816d84b22084d957471108ef2e545
glaukiol1 <glaukiol1@gmail.com>
2022-01-13 19:07:21 +0100

    add some basic MessageBuilder funcs

--- com/MessageBuilder.go
+++ com/MessageBuilder.go
@@ -6,3 +6,9 @@ package com
 // or request the chain
 // to be ready to broadcast
 //TODO-<
+
+func TYPE_GET_CHAIN() string {
+	return "TYPE_GET_CHAIN full_chain"
+}
+
+func TYPE_SEND_TRANSACTION() // todo
