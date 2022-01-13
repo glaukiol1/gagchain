bdec5ab73b880a6665f73cdffaf15274b0b161dd
glaukiol1 <glaukiol1@gmail.com>
2022-01-13 19:14:49 +0100

    db

---
+++ db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import "os"
+
+type DB struct {
+	location string
+}
+
+func (db *DB) InitDatabase(location string) {
+	db.location = location
+}
+
+func (db *DB) GetContents() string {
+	if db.location == "" {
+		panic("Set up the DB first")
+	}
+	dat, err := os.ReadFile(db.location)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(dat)
+}
+
+func (db *DB) Write(data string) {
+	dat := []byte(data)
+	err := os.WriteFile(db.location, dat, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
