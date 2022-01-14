9ca5b55a1199990f21801547d5a89687bd6f6a9e
glaukiol1 <glaukiol1@gmail.com>
2022-01-14 18:02:41 +0100

    basic transaction pool creator

--- blockchain/transaction_pool.go
+++ blockchain/transaction_pool.go
@@ -5,3 +5,13 @@ package blockchain
 // --- mine transactions
 // --- --- Part of this is checking the signature
 // --- sort transactions (by gas fee)
+
+const MIN_transactions_in_block = 2 // this is excluding mint address
+
+type TransactionPool struct {
+	pool []*Transaction
+}
+
+func NewTransactionPool() *TransactionPool {
+	return &TransactionPool{}
+}
