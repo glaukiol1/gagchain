25cef90869c8b6c1e5afb9ebbcf6e2212d92aff0
glaukiol1 <glaukiol1@gmail.com>
2022-01-14 18:15:16 +0100

    Mine transactions

--- blockchain/transaction_pool.go
+++ blockchain/transaction_pool.go
@@ -1,10 +1,12 @@
 package blockchain
 
+import "fmt"
+
 // ability to;
 // --- add transactions
 // --- mine transactions
-// --- edit transaction
 // --- --- Part of this is checking the signature
+// --- edit transaction
 // --- sort transactions (by gas fee)
 
 const MIN_transactions_in_block = 2 // this is excluding mint address
@@ -25,4 +27,26 @@ func (tp *TransactionPool) AddTransaction(transaction *Transaction) int {
 
 func (tp *TransactionPool) EditTransaction(transactionPos int, newTransaction *Transaction) {
 	tp.pool[transactionPos] = newTransaction
+}
+
+func (tp *TransactionPool) MineTransactions(isMiningNode bool) []*Transaction {
+	var blockData []*Transaction
+	if len(tp.pool) < MIN_transactions_in_block {
+		return []*Transaction{}
+	}
+	for i := 0; i < len(tp.pool); i++ {
+		trns := tp.pool[i]
+		if !(trns.IsReady()) {
+			// do something if it isnt ready
+		} else {
+			if !(trns.VerifySignature()) {
+				// do something if the signature is invalid
+				fmt.Printf("Transaction %s has an invalid signature", trns.GetTransactionJSON())
+			} else {
+				// mine transaction
+				blockData = append(blockData, trns)
+			}
+		}
+	}
+	return blockData
 }
